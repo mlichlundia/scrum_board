82d8dc65530d5177a19ec075b1a221a954f04039
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-01-15 21:09:47 +0100

    add login request & save token in session storage

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,20 +1,68 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { API_BASE_URL } from '../../constants/api.const'
+
 export default function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+      .post(`${API_BASE_URL}/auth/login`, {
+        username,
+        password,
+      })
+      .then((res) => {
+        console.log(res)
+        sessionStorage.setItem('token', res.data.access_token)
+        console.log(sessionStorage)
+        alert('Все хорошо')
+      })
+      .catch((err) => {
+        console.error(err)
+        alert('Все плохо')
+      })
+  }
+  function handleChange(e) {
+    e.target.name === 'username'
+      ? setUsername(e.target.value)
+      : setPassword(e.target.value)
+  }
+
   return (
     <div>
-      <form method="post" autocomplete="on" className="form registartion_">
+      <form
+        method="post"
+        autoComplete="on"
+        className="form login"
+        onSubmit={handleSubmit}
+      >
         <h3>Login</h3>
         <section>
           <div className="form-field username-form-field">
-            <label htmlFor="name">
+            <label htmlFor="username">
               <h5>Name</h5>
             </label>
-            <input type="text" placeholder="Name" required></input>
+            <input
+              name="username"
+              type="text"
+              placeholder="Name"
+              onChange={handleChange}
+              required
+            ></input>
           </div>
           <div className="form-field password-form-field">
             <label htmlFor="password">
               <h5>Password</h5>
             </label>
-            <input type="password" placeholder="Password" required></input>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+              required
+            ></input>
           </div>
         </section>
         <section>
